159a89395ec2741d0a4473f87b66e646995a6154
Khalatska <135690966+Khalatska@users.noreply.github.com>
2024-04-08 17:03:18 +0200

    api request

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { NavLink, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import MoviesPage from "./pages/MoviesPage/MoviesPage";
+import clsx from "clsx";
+import css from "./App.module.css";
 function App() {
-  return <div></div>;
+  const activeLink = ({ isActive }) => {
+    return clsx(css.link, isActive && css.active);
+  };
+
+  return (
+    <div>
+      <header>
+        <nav>
+          <NavLink to="/" className={activeLink}>
+            Home
+          </NavLink>
+          <NavLink to="/movies" className={activeLink}>
+            Movies
+          </NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const url = "https://api.themoviedb.org/3/trending/movie/day?language=en-US";
+
+const options = {
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzY0NTYwN2IyNmE1YjA0ODE0MWZkYWYzY2U4NWY0YyIsInN1YiI6IjY2MTNmN2E1NTQzN2Y1MDE0YTdkNGMyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NF4U_D4dywKt41-AaDzOG0Q5wjqAFjzEnucWNzcb6k8",
+  },
+};
+
+export const fetchTopMovies = async () => {
+  const res = await axios.get(url, options);
+  return res.data;
+};
+
+fetchTopMovies()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
